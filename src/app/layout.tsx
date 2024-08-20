e5ddd7fe518dc5f07a4f9c09248a901a52b5c01f
Dibbayajyoti <125145390+DibbayajyotiRoy@users.noreply.github.com>
2024-08-20 12:59:21 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Lunar Site",
+  description: "Space Mining Site Prediction with Tensor Flow and NextJs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={twMerge(inter.className, "bg-black text-white antialiased")}>
+        {children}
+      </body>
+    </html>
+  );
+}
